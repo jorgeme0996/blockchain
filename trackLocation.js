bcec7da83fcabc0369ad759a4792865e3c79bb31
Jorge Martinez <36254865+jorgeme0996@users.noreply.github.com>
2020-04-07 20:22:58 -0500

    Add files via upload

---
+++ trackLocation.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const URL = "https://www.ipapi.co/";
+
+const trackIp = (ip) => {
+    const urlToRequest = `${URL}/${ip}/json`;
+    request({
+        url: urlToRequest,
+        json: true
+    }, (err, response, body)=> {
+        if(!err && response.statusCode == 200) {
+            return {status: true, body }
+        } else {
+            return {statu: false, error: err }
+        } 
+
+    })
+}
+
+module.exports = trackIp;
